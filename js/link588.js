0b0bfbcc423dca33ae6f788667db05dc5cbef9ac
suyuwen1 <suyuwen1@126.com>
2016-04-05 22:43:31 +0800

    2016.4.5 j

--- js/link588.js
+++ js/link588.js
@@ -1,5 +1,6 @@
 $(function(){
     toplisthover();
+    highlighter();
 });
 
 function toplisthover(){
@@ -15,4 +16,42 @@ function toplisthover(){
             $(".toplistm").hide();
         }
     );
+}
+function path() {
+    var args = arguments,
+    result = [];
+    for (var i = 0; i < args.length; i++)
+        result.push(args[i].replace('$', templetspath+'/style/syntaxhighlighter/scripts/'));
+    return result;
+}
+function highlighter(){
+    // if (!IsMobileSys) {
+        SyntaxHighlighter.autoloader.apply(null, path(
+        'applescript            $shBrushAppleScript.js',
+        'actionscript3 as3      $shBrushAS3.js',
+        'bash shell             $shBrushBash.js',
+        'coldfusion cf          $shBrushColdFusion.js',
+        'cpp c                  $shBrushCpp.js',
+        'c# c-sharp csharp      $shBrushCSharp.js',
+        'css                    $shBrushCss.js',
+        'delphi pascal          $shBrushDelphi.js',
+        'diff patch pas         $shBrushDiff.js',
+        'erl erlang             $shBrushErlang.js',
+        'groovy                 $shBrushGroovy.js',
+        'java                   $shBrushJava.js',
+        'jfx javafx             $shBrushJavaFX.js',
+        'js jscript javascript  $shBrushJScript.js',
+        'perl pl                $shBrushPerl.js',
+        'php                    $shBrushPhp.js',
+        'text plain             $shBrushPlain.js',
+        'py python              $shBrushPython.js',
+        'ruby rails ror rb      $shBrushRuby.js',
+        'sass scss              $shBrushSass.js',
+        'scala                  $shBrushScala.js',
+        'sql                    $shBrushSql.js',
+        'vb vbnet               $shBrushVb.js',
+        'xml xhtml xslt html    $shBrushXml.js'
+        ));
+        SyntaxHighlighter.all();
+    // }
 }
