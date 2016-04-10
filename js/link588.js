8a89a039e95712e9a17f43beed0bc784802659bc
suyuwen1 <suyuwen1@126.com>
2016-04-11 00:09:01 +0800

    2016.4.11 j

--- js/link588.js
+++ js/link588.js
@@ -55,3 +55,11 @@ function highlighter(){
         SyntaxHighlighter.all();
     }
 }
+//运行文本域代码
+function runEx(id) {
+    $val = $("#".id).value();
+    var TestWin=window.open('','',''); //打开一个窗口并赋给变量TestWin。
+    TestWin.opener = null // 防止代码对论谈页面修改
+    TestWin.document.write($val);//向这个打开的窗口中写入代码code，这样就实现了运行代码功能。
+    TestWin.document.close();
+}
