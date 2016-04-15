8dd0b6f9e78046f9909093c47938dd0609e9778c
suyuwen1 <suyuwen1@126.com>
2016-04-15 16:12:05 +0800

    2016.4.15 d

--- sqlregex/jb51.php
+++ sqlregex/jb51.php
@@ -4,6 +4,7 @@
     }
     $regex = array(); //最后一个必须是替换链接。
     //$regex[0] = '<p[^>]*>(.*)?运行效果(截图|图)(.*)?<\/p>';
+    /*
     $regex[1] = '<p>在线演示地址如下：<\/p>';
     $regex[2] = '<img[^>]*jb51\.net[^>]*>';
     $regex[3] = '<p[^>]*>(\r\n)*(.*)?demo\.jb51\.net(.*)?<\/p>';
@@ -15,7 +16,17 @@
     $regex[8] = '<input[^>]*运行代码[^>]*>';
     $regex[9] = '\s*title\s*=\s*\"[^\"]*\"';
     $regex[10] = '\s*alt\s*=\s*\"[^\"]*\"';
-    $regex[11] = 'jb51\.net';
+    $regex[11] = 'jb51\.net';*/
+    
+    $regex[0] = '<p>在线演示地址如下：<\/p>';
+    $regex[1] = '<img[^>]*jb51\.net[^>]*>';
+    $regex[2] = '<p[^>]*>(\r\n)*(.*)?demo\.jb51\.net(.*)?<\/p>';
+    $regex[3] = '<span(.*)?><u>复制代码<\/u><\/span>';
+    $regex[4] = '<br \/>复制代码';
+    $regex[5] = '<input[^>]*(复制代码|另存代码|运行代码)[^>]*>';
+    $regex[6] = 'alt\s*=\s*(\'|\")[^\'\"]*\1';
+    $regex[7] = 'title\s*=\s*(\'|\")[^\'\"]*\1';
+    $regex[8] = 'jb51\.net';
     
     $rep = array();
     $where = "body REGEXP '";
@@ -25,11 +36,13 @@
             $rep[$key] = 'link588.com';
         }else{
             $where .= "$val|";
-            if ($key == 9 || $key == 10) {
-                $rep[$key] = 'link588.com';
+            if ($key == 6) {
+                $rep[$key] = 'alt="link588.com"';
+            }else if($key == 7){
+                $rep[$key] = 'title="link588.com"';
             }else{
                 $rep[$key] = '';
-            }            
+            }
         }
         $regex[$key] = '/'.$val.'/i';
     }
@@ -51,31 +64,22 @@
 	if($result && mysqli_num_rows($result)){
         while($row = mysqli_fetch_assoc($result)){
             $body = preg_replace($regex, $rep, $row['body']);
-            //echo $body.'</br>';
             if ($body != $row['body']) {
-                $body = mysqli_real_escape_string($M->mysql,$body);
-                $sql = "UPDATE `dede_addonarticle` SET `body` = '$body' WHERE aid = {$row['aid']}";
-                $result2=mysqli_query($M->mysql,$sql);
-			    if($result2 && mysqli_affected_rows($M->mysql)>0){
-                    echo '<span class="num">'.$row['aid'].'</span><span style="color:green;">替换成功！</span></br>';
+                if($body != ''){
+                    $body = mysqli_real_escape_string($M->mysql,$body);
+                    $sql = "UPDATE `dede_addonarticle` SET `body` = '$body' WHERE aid = {$row['aid']}";
+                    $result2=mysqli_query($M->mysql,$sql);
+                    if($result2 && mysqli_affected_rows($M->mysql)>0){
+                        echo '<span class="num">'.$row['aid'].'</span><span style="color:green;">替换成功！</span></br>';
+                    }else{
+                        echo '<span class="num">'.$row['aid'].'</span><span style="color:red;">替换失败！</span></br>';
+                    }
                 }else{
-                    echo '<span class="num">'.$row['aid'].'</span><span style="color:red;">替换失败！</span></br>';
+                    echo '<span class="num">'.$_POST['s'].'</span><span style="color:red;">内容被替换为空！但未写入数据库！</span></br>';
                 }
             }else{
                 echo '<span class="num">'.$_POST['s'].'</span><span style="color:#777708;">不需要替换！</span></br>';
             }
-            
-            
-            
-            // $result2=mysqli_query($M->mysql,$sql);
-			// if($result2 && mysqli_affected_rows($M->mysql)>0){
-            //     echo '替换成功！</br>';
-            // }else{
-            //     echo $row['aid'].'------------------------------------替换失败！----------------------------------------<br>';
-            //     echo mysqli_error($M->mysql);
-            //     echo '<br>';
-            //     echo '<pre>'.$row['body'].'</pre><br>';
-            // }
         }
     }else{
         echo '<span class="num">'.$_POST['s'].'</span><span style="color:#666;">没有要替换的数据！</span></br>';        
