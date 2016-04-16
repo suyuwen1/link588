fd23da8d78f32cdd7b2fe9c626a06a2d956d98b8
suyuwen1 <suyuwen1@126.com>
2016-04-16 18:17:09 +0800

    2016.4.16 j

--- js/link588.js
+++ js/link588.js
@@ -57,7 +57,7 @@ function highlighter(){
 }
 //运行文本域代码
 function runEx(id) {
-    $val = $("#".id).value();
+    $val = $("#".id).val();
     var TestWin=window.open('','',''); //打开一个窗口并赋给变量TestWin。
     TestWin.opener = null // 防止代码对论谈页面修改
     TestWin.document.write($val);//向这个打开的窗口中写入代码code，这样就实现了运行代码功能。
