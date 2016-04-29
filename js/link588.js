d07993c6bde1f0ce6129bf1c4a5abe31dbc5393c
suyuwen1 <suyuwen1@126.com>
2016-04-29 15:45:21 +0800

    2016.4.29 d

--- js/link588.js
+++ js/link588.js
@@ -7,7 +7,7 @@ function toplisthover(){
     $(".navMenu ul li").hover(
         function(){
             //console.log($(this).index(),$(".navMenu ul li").length);
-            if($(this).index() == $(".navMenu ul li").length-1){
+            if($(this).index() == $(".navMenu ul li").length-2){
                 $(this).children(".toplistm").css({right:'-1px',left:'auto'});
             }
             $(this).children(".toplistm").show();
