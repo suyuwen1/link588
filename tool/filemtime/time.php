650d8f54898d3b37015ed0a3cc1422fc93b1a49e
suyuwen1 <suyuwen1@126.com>
2016-05-23 10:24:59 +0800

    2016.5.23 d

--- tool/filemtime/time.php
+++ tool/filemtime/time.php
@@ -8,12 +8,15 @@
     <title>Document</title>
 </head>
 <body>
-    <font color="red">在地址里输入得到时间</font> ?time=1462763348
+    <font color="red">在地址里输入得到时间</font> ?time=1462763348&str=2016-4-28
 <?php
     echo '<p>现在时间：'.time().'</p>';
     echo '<p>现在时间：'.date('Y-m-d H:i:s',time()).'</p>';
     if(!empty($_GET['time'])){
-        echo '<p>得到时间：'.date('Y-m-d H:i:s',$_GET['time']).'</p>';
+        echo '<p>time得到时间：'.date('Y-m-d H:i:s',$_GET['time']).'</p>';
+    }
+    if(!empty($_GET['str'])){
+        echo '<p>str得到时间：'.strtotime($_GET['str']).'</p>';
     }
 ?>
 </body>
