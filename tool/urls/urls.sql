d40c16e1bef510b860c63f837c3b8466cec4e8e3
suyuwen1 <suyuwen1@126.com>
2016-06-05 18:13:49 +0800

    2016.6.5 j

---
+++ tool/urls/urls.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version phpStudy 2014
+-- http://www.phpmyadmin.net
+--
+-- 主机: localhost
+-- 生成日期: 2016 年 06 月 05 日 18:12
+-- 服务器版本: 5.5.47
+-- PHP 版本: 5.3.29
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 数据库: `urls`
+--
+CREATE DATABASE `urls` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `urls`;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `urls`
+--
+
+CREATE TABLE IF NOT EXISTS `urls` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `url` varchar(600) NOT NULL DEFAULT '0',
+  `email` varchar(300) NOT NULL DEFAULT '0',
+  `ctime` varchar(50) NOT NULL DEFAULT '0',
+  `hf` int(5) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `url` (`url`(333)),
+  KEY `email` (`email`),
+  KEY `hf` (`hf`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
