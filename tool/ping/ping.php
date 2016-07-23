0ca1981f17c11e049d98618bd45486b7a4b0b6a3
suyuwen1 <suyuwen1@126.com>
2016-07-23 19:33:57 +0800

    2016.7.23 j

--- tool/ping/ping.php
+++ tool/ping/ping.php
@@ -18,14 +18,58 @@
     }
 
     $urls = array();
+    $urls2 = array();
+    $num = 0;
+    $back = array();
+    $back['success'] = 0;
+    $result2 = '';
     $result = mysqli_query($M->mysql,$sql);
+    $back['total'] = mysqli_num_rows($result);
     if($result && mysqli_num_rows($result)){
+        
         while($row = mysqli_fetch_assoc($result)){
             $row['namerule'] = str_replace('{aid}',$row['id'],$row['namerule']);
             $urls[] = 'http://www.link588.com'.$row['namerule'];
-        }
+            // $num++;
 
-        $api = 'http://data.zz.baidu.com/urls?site=www.link588.com&token=cjec0HNMvsZfppqp';
+            // if ($back['total']<2000) {
+            //     baidu_ping($urls);
+            // }
+
+            // if ($num == 2000) {//百度一次最多推2000条
+            //     baidu_ping($urls);
+            //     $num = 0;
+            //     $urls = array();
+            // }
+        }        
+        
+        $page = ceil($back['total']/2000);
+        //echo $page.'</br>';
+        for ($i=0; $i < $page; $i++) {
+            $a = ($i == $page-1)?$back['total']:$i*2000+2000;
+            //echo $a.'</br>';
+            for ($ii=$i*2000; $ii <$a ; $ii++) {
+                $urls2[] = $urls[$ii];
+            }
+            //var_dump($urls2);
+            //echo '-------------------------';
+            //var_dump($i);
+            baidu_ping($urls2);
+            $urls2 = array();
+            sleep(3);
+        }
+        
+        
+    }else{
+        echo mysqli_error($M->mysql);
+        //echo mysqli_num_rows($result);
+    }
+echo json_encode($back);
+
+function baidu_ping($urls='')
+{
+    global $back, $result2;
+    $api = 'http://data.zz.baidu.com/urls?site=www.link588.com&token=cjec0HNMvsZfppqp';
         $ch = curl_init();
         $options =  array(
             CURLOPT_URL => $api,
@@ -39,18 +83,23 @@
         curl_close($ch);
         $result = json_decode($result,true);
         if($result['success']){
-            if($_POST['id'] == 'add' || $_POST['id'] == 'all'){
+
+            $back['success'] += $result['success'];
+            $back['remain'] = $result['remain'];
+
+            if(($_POST['id'] == 'add' || $_POST['id'] == 'all') && empty($result2)){
                 $sql = "UPDATE `ping` SET `last_id` = (SELECT id FROM `dede_archives` ORDER BY id DESC limit 1)";
                 $result2 = mysqli_query($M->mysql,$sql);
                 if($result2 && mysqli_affected_rows($M->mysql)>0){
-                    $result['ping'] = '最后更新ID已更新！';
+                    $back['ping'] = '最后更新ID已更新！';
                 }else{
-                    $result['ping'] = '最后更新ID更新失败！';                    
+                    $back['ping'] = '最后更新ID更新失败！';                    
                 }
             }
+
+        }else{
+            $back['error'] = $result['error'].':'.$result['message'];
+            $back['error']['url'][] = $urls;
         }
-        
-        $result['total'] = count($urls);
-        echo json_encode($result);
-    }
+}
 ?>
